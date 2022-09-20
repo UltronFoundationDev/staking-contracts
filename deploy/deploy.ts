f7c021a4680fef1f7b6726aa3f8bdf5a53bda3b2
backendultron <backend@ultron.foundation>
2022-09-20 18:00:52 +0300

    upd(pavel)

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,5 +1,5 @@
-import { BigNumber } from "ethers";
-import { subtask, task, types } from "hardhat/config";
+import {BigNumber} from "ethers";
+import {task, types} from "hardhat/config";
 import * as Helpers from './helpers';
 
 async function deployContract(ethers: any, name: string, ...params: any[]) {
@@ -9,15 +9,35 @@ async function deployContract(ethers: any, name: string, ...params: any[]) {
     console.log(`${name}: ${contract.address}`);
 }
 
+require('dotenv').config();
+
+
+const fs = require('fs');
+
+// const filename = process.env.DIRNAME + "/deployed_storage.json";
+const filename = "./deployed_storage.json";
+
+let deployed_storage: any = {};
+try {
+  deployed_storage = JSON.parse(fs.readFileSync(filename).toString().trim());
+  console.log(deployed_storage);
+} catch (err) {
+  console.log("No ", filename, ' Let\'s deploy contracts');
+}
+
+
 task("deploy", "Deploy Staking Contracts")
   .setAction(async (taskArgs, { ethers, run }) => {
-    const wulx = '0x3a4F06431457de873B588846d139EC0d86275d54';
+    const wulx = JSON.parse(fs.readFileSync(filename).toString().trim())["wulx"];
     
     const xulx = await run("xulx", { wulx: wulx });
+    const acelab = await run("acelab", {xulx: xulx});
+    const brewulx = await run("brewulx", {xulx: xulx, wulx: wulx});
 
-    const acelab = await run("acelab", { xulx: xulx });
-
-    const brewulx = await run("brewulx", { xulx: xulx, wulx: wulx });
+    deployed_storage["xulx"] = xulx;
+    deployed_storage["acelab"] = acelab;
+    deployed_storage["brewulx"] = brewulx;
+    fs.writeFileSync(filename, JSON.stringify(deployed_storage));
   });
 
 task("xulx", "The contract xulx is deployed")
@@ -50,9 +70,9 @@ task("brewulx", "The contract BrewUlx is deployed")
   .setAction(async (taskArgs, { ethers, network }) => {
         const signer = (await ethers.getSigners())[0];
 
-        const factoryAddress = '0xe1F0D4a5123Fd0834Be805d84520DFDCd8CF00b7';
-        const route1Address = '0x97FDd294024f50c388e39e73F1705a35cfE87656'; // usdt
-        const route2Address = '0x3c4E0FdeD74876295Ca36F62da289F69E3929cc4'; // usdc
+        const factoryAddress = JSON.parse(fs.readFileSync(filename).toString().trim())["UniswapV2Factory"];
+        const route1Address = JSON.parse(fs.readFileSync(filename).toString().trim())["uUSDT"]; // usdt
+        const route2Address = JSON.parse(fs.readFileSync(filename).toString().trim())["uUSDC"]; // usdc
 
         const brewUlxFactory = await ethers.getContractFactory("BrewULX", signer);
         const brewUlx = await (await brewUlxFactory.deploy(factoryAddress, taskArgs.xulx, taskArgs.wulx, route1Address, route2Address)).deployed();
@@ -65,24 +85,24 @@ task("add-tokens", "Adding tokens to aceLab")
   .setAction(async (taskArgs, { ethers, run }) => { 
     const signer = (await ethers.getSigners())[0];
 
-    const aceLabAddress = '0xa7cF49a0C559414d51ae607ccc1563D292ECf23A';
+    const aceLabAddress = JSON.parse(fs.readFileSync(filename).toString().trim())["acelab"];
     const aceLab = await ethers.getContractAt("AceLab", aceLabAddress, signer);
     
-    const treasuryAddress = "0xD60e1D7CCf2Bb8E2052079914c333c92D687B965";
+    const treasuryAddress = signer.address;
 
-    const wbtc  = '0xd2b86a80A8f30b83843e247A50eCDc8D843D87dD';
-    const weth  = '0x2318Bf5809a72AaBAdd15a3453A18e50Bbd651Cd';
-    const bnb   = '0x169ac560852ed79af3D97A8977DCf2EBA54A0488';
-    const avax  = '0x6FE94412953D373Ef464b85637218EFA9EAB8e97';
-    const busd  = '0xc7cAc85C1779d2B8ADA94EFfff49A4754865e2E4';
-    const shib  = '0xb5Bb1911cf6C83C1a6E439951C40C2949B0d907f';
-    const matic = '0x6094a1e3919b302E236B447f45c4eb2DeCE9D9F4';
-    const ftm   = '0xE8Ef8A6FE387C2D10951a63ca8f37dB6B8fA02C1';
-    const dai   = '0x045F0f2DE758743c84b756B1Fca735a0dDf0b8f4';
-    const link  = '0xc8Fb7999d62072E12fE8f3EDcd7821204FCa0344';
-    const usdt  = '0x97FDd294024f50c388e39e73F1705a35cfE87656';
-    const usdc  = '0x3c4E0FdeD74876295Ca36F62da289F69E3929cc4';
-    const wulx  = '0x3a4F06431457de873B588846d139EC0d86275d54';
+    const wbtc  = JSON.parse(fs.readFileSync(filename).toString().trim())["wBTC"];
+    const weth  = JSON.parse(fs.readFileSync(filename).toString().trim())["wETH"];
+    const bnb   = JSON.parse(fs.readFileSync(filename).toString().trim())["bnb"];
+    const avax  = JSON.parse(fs.readFileSync(filename).toString().trim())["avax"];
+    const busd  = JSON.parse(fs.readFileSync(filename).toString().trim())["bUSD"];
+    const shib  = JSON.parse(fs.readFileSync(filename).toString().trim())["shib"];
+    const matic = JSON.parse(fs.readFileSync(filename).toString().trim())["matic"];
+    const ftm   = JSON.parse(fs.readFileSync(filename).toString().trim())["ftm"];
+    const dai   = JSON.parse(fs.readFileSync(filename).toString().trim())["dai"];
+    const link  = JSON.parse(fs.readFileSync(filename).toString().trim())["link"];
+    const usdt  = JSON.parse(fs.readFileSync(filename).toString().trim())["uUSDT"];
+    const usdc  = JSON.parse(fs.readFileSync(filename).toString().trim())["uUSDC"];
+    const wulx  = JSON.parse(fs.readFileSync(filename).toString().trim())["wulx"];
 
     const dateNow = Math.floor(Date.now() / 1000);
     const tokens = [usdc, avax, dai, wulx, shib];
@@ -91,9 +111,6 @@ task("add-tokens", "Adding tokens to aceLab")
         await aceLab.add(rewardPerSecond, tokens[i], BigNumber.from(dateNow).sub(1000), BigNumber.from(dateNow).mul(2), treasuryAddress, { gasLimit: 3000000 });
         console.log(`POOL ${i} | ${tokens[i]} | ${rewardPerSecond}`);
         Helpers.delay(4000);
-    }
-    for(let i:number = 0; i < tokens.length; i++) {
-      console.log(await aceLab.poolInfo(i));
     }
   });
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -50,78 +50,9 @@ const config: HardhatUserConfig = {
     artifacts: "./artifacts"
   },
   networks: {
-    hardhat: {
-      chainId: 1337,
-      forking: {
-        url: "https://eth-mainnet.g.alchemy.com/v2/i2LgfhBeI-JidguSNlFuToo7kPSkFBPb",
-        blockNumber: 11095000,
-      },
-      gas: 2100000,
-      gasPrice: 8000000000,
-    },
-    ultron: {
-      url: `https://ultron-rpc.net`,
-      chainId: 1231,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    ethereum: {
-      url: "https://eth-mainnet.g.alchemy.com/v2/i2LgfhBeI-JidguSNlFuToo7kPSkFBPb",
-      chainId: 1,
-      gasPrice: 30000000000,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    bsc: {
-      url: "https://bsc-dataseed1.binance.org/",
-      chainId: 56,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    avalanche: {
-      url: "https://api.avax.network/ext/bc/C/rpc",
-      chainId: 43114,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    polygon: {
-      url: "https://rpc-mainnet.matic.quiknode.pro/",
-      chainId: 137,
-      gasPrice: 40000000000,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    fantom: {
-      url: "https://rpc.ftm.tools/",
-      chainId: 250,
-      gasPrice: 50000000000,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    ultron_testnet: {
-      url: `https://ultron-dev.io`,
-      chainId: 1230,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    goerli: {
-      url: `${process.env.API_URL}`,
-      chainId: 5,
-      gas: 2100000,
-      gasPrice: 8000000000,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    bsc_testnet: {
-      url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
-      chainId: 97,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    avalanche_fuji: {
-      url: "https://api.avax-test.network/ext/bc/C/rpc",
-      chainId: 43113,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    }, 
-    mumbai: {
-      url: "https://rpc-mumbai.matic.today",
-      chainId: 80001,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    }, 
-    fantom_testnet: {
-      url: `https://rpc.testnet.fantom.network/`,
-      chainId: 4002,
+    ganache_ultron: {
+      url: `http://ganache_ultron:8545`,
+      chainId: 1000,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
